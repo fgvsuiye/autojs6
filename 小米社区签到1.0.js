a8056ddc15c78101ce38b91a964b9fc37cd588d5
fgvsuiye <166627411+fgvsuiye@users.noreply.github.com>
2025-03-27 11:03:42 +0800

    Update 小米社区签到1.0.js
    Signed-off-by: fgvsuiye <166627411+fgvsuiye@users.noreply.github.com>

--- 小米社区签到1.0.js
+++ 小米社区签到1.0.js
@@ -58,12 +58,18 @@ function unLock() {
 
 //关闭程序
 function killAPP(packageName){
-    app.openAppSetting(packageName)
-    wait(() => textContains("结束运行"||"强行停止").findOne(3000),10,500,{
+    wait(() => {
+        app.openAppSetting(packageName)
+        return textContains("结束运行"||"强行停止").findOne(1000);
+    },2,500,{
         then(){
             click("结束运行"||"强行停止");
-            while(click("确定"));
-            log("结束小米社区");
+            if (textContains("确定").findOne(1500)) {
+                click("确定");
+                log("结束小米社区");
+            }else{
+                log("程序未运行");
+            }
             sleep(500);
         },
         else(){
@@ -104,7 +110,7 @@ function posts(){
             wait(() => {
                 log("尝试打开帖子");
                 textView.click();
-                return className("android.widget.TextView").text("收藏").findOne(5000);
+                return className("android.widget.TextView").textContains("收藏").findOne(5000);
             }, 3, 1000, {
                 then(){
                     log("打开帖子成功,开始浏览");
@@ -117,9 +123,6 @@ function posts(){
                 }
             });
         },
-        else(){
-            log("未找到帖子页面，退出");
-        }
     })
 }
 
@@ -687,6 +690,7 @@ function main() {
     if (config.浏览帖子) posts();
     wait(() => signView(), 3, 1000,{
         then(){
+            //percentage = logpercentage();
             sign();
             // 按配置启用功能
             if (config.双旗舰) 活动1();
